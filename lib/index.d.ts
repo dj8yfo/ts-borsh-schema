7c01b4f8188676d0caf464e7605b4f056a8ce762
dj8yf0μl <sffhox@gmail.com>
2022-11-09 16:54:54 +0200

    remove redundant fields

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,3 @@
-import { PublicKey } from '@velas/web3';
-export declare class PublicKeyBE {
-    solPubKey: PublicKey;
-    value: Uint8Array;
-    constructor(fields: {
-        value: Uint8Array;
-    });
-    fromPubKey(arg: PublicKey): this;
-    equals(publicKey: PublicKeyBE): boolean;
-}
-export declare const borshPublicKeyHack: () => void;
 export declare class Struct {
     constructor(properties: any);
 }
